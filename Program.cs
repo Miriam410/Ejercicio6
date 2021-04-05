9ff7bece91b682fea5fb70957220e7e064a01c60
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-05 16:14:18 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 6. Lea las teclas presionadas y finalice al presionar Shift+ Ctrl+ F
+            ConsoleKeyInfo tecla;
+            do
+            {
+                Console.WriteLine("Ingrese una tecla");
+                char letra = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine($"Usted ha presionado la tecla  {letra} ");
+                Console.WriteLine("Para finalizar presione Shift + Ctrl + F");
+                tecla = Console.ReadKey();
+                if (((tecla.Modifiers & ConsoleModifiers.Shift)!=0) &&
+                    ((tecla .Modifiers & ConsoleModifiers.Control)!=0) &&
+                        (tecla.Key == ConsoleKey.F))
+                {
+                    break;
+                }
+            } while(true);
+        }
+    }
+}
